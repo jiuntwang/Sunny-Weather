e91ee0edcd045793a19f7ff763fa9ce504e1d0ed
Jiun Wang <jiuntwang@gmail.com>
2017-05-16 12:13:48 -0700

    Installed Angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sunnyWeatherApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sunnyWeatherApp
+ */
+angular.module('sunnyWeatherApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
